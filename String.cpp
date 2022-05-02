46e508e1fbd5ec7dc5ff23841d6c6277242c12d3
Bhavesh <bhavesh.raina03@yahoo.com>
2022-05-03 04:29:11 +0530

    13.Strings and .gitignore for .exe added

---
+++ String.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // to append two strings and store the result in one of them use s1.append(s2); OR s1 += s2;
+    // to clear all the characters stored in a string use s1.clear();
+    // to compare s1 with s2 use s1.compare(s2);
+    // to check if string is empty use s1.empty();
+    // to get the position of a set of chars in a string use s1.find("poo");
+    // to insert str in a string at the index n use str.insert(n,"str");
+    // to get the length of a string use either str.length(); [used with strings] OR str.size();
+    // to get the substring (m chars) of a string form nth index use str.substr(n, m);
+    // to convert a numeric str to int use stoi(s1); and for a numeric array(with terminator char) to int use atoi(s1); OR fast_atoi(s1);
+    // to convert x into a string use to_string(x);
+    // to sort a string use the same sort(arr, arr+i); func like sort(str.begin(), str.end());
+    // to manipulate a string/array use transform(); [gives more control than toupper() and tolower()]
+    cout<<"hellow world";
+    return 0;
+}
